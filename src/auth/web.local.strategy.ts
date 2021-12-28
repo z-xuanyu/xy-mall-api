b20c888bc0a3052b853b11cbf57419b436c8259a
xuanyu <969718197@qq.com>
2021-12-28 18:06:24 +0800

    🎉 : 修复登录错误统一格式返回

--- src/auth/web.local.strategy.ts
+++ src/auth/web.local.strategy.ts
@@ -4,16 +4,16 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2021-12-24 17:20:13
- * @LastEditTime: 2021-12-27 14:22:56
+ * @LastEditTime: 2021-12-28 18:03:10
  * @Description: web站会员登录策略
  */
 import { Strategy, IStrategyOptions } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectModel } from 'nestjs-typegoose';
 import { ReturnModelType } from '@typegoose/typegoose';
-import { BadRequestException } from '@nestjs/common';
 import { compareSync } from 'bcryptjs';
 import { User } from '@app/db/modules/user.model';
+import { ApiFail } from '@app/common/ResponseResultModel';
 
 export class WebLocalStrategy extends PassportStrategy(Strategy, 'web-local') {
   constructor(
@@ -29,13 +29,13 @@ export class WebLocalStrategy extends PassportStrategy(Strategy, 'web-local') {
   async validate(email: string, password: string): Promise<User> {
     const user = await this.userModel.findOne({ email }).select('+password');
     if (!user) {
-      throw new BadRequestException('用户名不正确');
+      throw new ApiFail(101, '用户名不正确');
     }
     if (!compareSync(password, user.password)) {
-      throw new BadRequestException('密码不正确');
+      throw new ApiFail(102, '密码不正确');
     }
     if (!user.status) {
-      throw new BadRequestException('用户已被禁用');
+      throw new ApiFail(103, '用户已被禁用');
     }
     return user;
   }
