8082c6dc94e14533ef41ee34b9a79852e912d2de
xuanyu <969718197@qq.com>
2022-02-10 15:15:32 +0800

    🙈：修改数据库连接字符串

--- libs/common/src/config/db.config.ts
+++ libs/common/src/config/db.config.ts
@@ -4,10 +4,23 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-01-06 10:49:12
- * @LastEditTime: 2022-01-06 10:51:45
- * @Description: Modify here please
+ * @LastEditTime: 2022-02-10 15:05:32
+ * @Description: 数据库链接字符串配置
  */
 
+const baseConfig = {
+  // 数据库地址
+  host: '175.178.107.120',
+  // 端口
+  port: '27017',
+  // 用户名
+  userName: 'jp-admin',
+  // 密码
+  password: 'Yushao,19970415.',
+  // 数据库名称
+  dbName: 'nest-jp',
+};
+
 export const dbConfig = {
-  url: 'mongodb://admin:Yushao,19970415.@175.178.107.120:27017/nest-jp?authSource=admin',
+  url: `mongodb://${baseConfig.userName}:${baseConfig.password}@${baseConfig.host}:${baseConfig.port}/${baseConfig.dbName}?authSource=admin`,
 };
--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2021-12-24 15:39:34
- * @LastEditTime: 2021-12-28 10:00:44
+ * @LastEditTime: 2022-02-10 15:12:40
  * @Description: 管理员控制器
  */
 import { ParseIdPipe } from '@app/common/pipe/parse-id.pipe';
