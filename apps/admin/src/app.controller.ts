1567dce8ce72b5f20eab96320334a38c6cfeeb18
xuanyu <969718197@qq.com>
2022-03-18 12:06:47 +0800

    🙈：添加公众号配置项

--- apps/admin/src/app.controller.ts
+++ apps/admin/src/app.controller.ts
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-03-03 09:54:20
- * @LastEditTime: 2022-03-16 14:14:16
+ * @LastEditTime: 2022-03-18 11:15:25
  * @Description: Modify here please
  */
 import {
@@ -12,6 +12,7 @@ import {
   Get,
   Post,
   Req,
+  Res,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -27,7 +28,11 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { apiSucceed } from 'libs/common/ResponseResultModel';
+import { ApiConfig, ApiConfigKit, WeChat } from 'tnwx';
 import { AppService } from './app.service';
+import { HandMsgAdapter } from './handMsgAdapter';
+import { Request, Response } from 'express';
+import * as getRawBody from 'raw-body';
 
 class FileUploadDto {
   @ApiProperty({ type: 'string', format: 'binary' })
@@ -37,7 +42,20 @@ class FileUploadDto {
 @ApiTags('首页')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 微信公众号消息处理
+  private msgAdapter = new HandMsgAdapter();
+  constructor(private readonly appService: AppService) {
+    // 初始化公众号配置
+    const devApiConfig = new ApiConfig(
+      'wxb1bcb0988520b14a',
+      '56e9a2ea5ffed507b3cbb8f9167ce516',
+      'xuanyutest',
+      false,
+      'Zw9VFJ2DtojjkuXlSheo9Qv3bGPLK9GrpcKNC3WTuM7',
+    );
+    ApiConfigKit.putApiConfig(devApiConfig);
+    ApiConfigKit.devMode = true;
+  }
 
   @Get()
   getHello(): string {
@@ -59,5 +77,28 @@ export class AppController {
     const domain = `${req.protocol}://${req.headers.host}`;
     const res = await this.appService.upload(file, domain);
     return apiSucceed(res?.url);
+  }
+
+  @Get('weixin')
+  getMsg(@Req() request: Request) {
+    const signature = request.query.signature.toString(), //微信加密签名
+      timestamp = request.query.timestamp.toString(), //时间戳
+      nonce = request.query.nonce.toString(), //随机数
+      echostr = request.query.echostr.toString(); //随机字符串
+    return WeChat.checkSignature(signature, timestamp, nonce, echostr);
+  }
+
+  @Post('weixin')
+  async PostMsg(@Res() res: Response) {
+    // const buffer: Buffer = await getRawBody(req);
+    // const msgXml = buffer.toString('utf-8');
+    // const data = await WeChat.handleMsg(this.msgAdapter, msgXml);
+    res.send(`<xml>
+    <ToUserName><![CDATA[o-bHRs8SmQTWQqBi2n1vatcrTAw8]]></ToUserName>
+    <FromUserName><![CDATA[gh_cfd61f5e4587]]></FromUserName>
+    <CreateTime>${new Date().getTime()}</CreateTime>
+    <MsgType><![CDATA[text]]></MsgType>
+    <Content><![CDATA[你好]]></Content>
+  </xml>`);
   }
 }
