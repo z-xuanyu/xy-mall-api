f43129a84b3f16c2689a8e6cc45f3119b6399ca8
ayu <969718197@qq.com>
2022-03-20 22:43:01 +0800

    🙈：添加商品评价模型

---
+++ libs/db/src/modules/product-comment-model.ts
@@ -0,0 +1,42 @@
+/*
+ * @Author: xuanyu
+ * @LastEditors: xuanyu
+ * @email: 969718197@qq.com
+ * @github: https://github.com/z-xuanyu
+ * @Date: 2022-03-20 10:56:11
+ * @LastEditTime: 2022-03-20 11:03:49
+ * @Description: 商品评价
+ */
+
+import { ApiProperty } from '@nestjs/swagger';
+import { ModelOptions, prop, Ref } from '@typegoose/typegoose';
+import { Product } from './product.model';
+import { User } from './user.model';
+
+// 添加创建时间、更新时间字段
+@ModelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
+export class ProductComment {
+  @ApiProperty({ title: '用户ID' })
+  @prop({ ref: () => User })
+  userId: Ref<User>;
+
+  @ApiProperty({ title: '商品ID' })
+  @prop({ ref: () => Product })
+  productId: Ref<Product>;
+
+  @ApiProperty({ title: '评分星级' })
+  @prop({ type: Number, default: 0 })
+  rate: number;
+
+  @ApiProperty({ title: '内容' })
+  @prop({ type: String })
+  content: string;
+
+  @ApiProperty({ title: '评论商品图片' })
+  @prop({ type: [String] })
+  images: Array<string>;
+}
