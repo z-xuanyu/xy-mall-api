eae28852e3c7db3c6b17ea0675bc470b7dd83a42
xuanyu <969718197@qq.com>
2022-03-21 17:14:12 +0800

    feat: 添加代码提交规范插件

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: xuanyu
+ * @LastEditors: xuanyu
+ * @email: 969718197@qq.com
+ * @github: https://github.com/z-xuanyu
+ * @Date: 2022-03-21 17:12:21
+ * @LastEditTime: 2022-03-21 17:12:22
+ * @Description: Modify here please
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
