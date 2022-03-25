fd6d35edbfe856b06c61cafa76bab0baf3901742
xuanyu <969718197@qq.com>
2022-03-25 18:16:40 +0800

    fix(menu): 修复权限菜单接口模型设计符合vben要求

--- apps/admin/src/menu/dto/create-menu.dto.ts
+++ apps/admin/src/menu/dto/create-menu.dto.ts
@@ -4,36 +4,72 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-03-25 12:16:37
- * @LastEditTime: 2022-03-25 14:31:44
+ * @LastEditTime: 2022-03-25 18:09:16
  * @Description: Modify here please
  */
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
-export class CreateMenuDto {
-  @ApiProperty({ title: '菜单名称' })
-  @IsNotEmpty({ message: '名称不能为空' })
-  name: string;
+class TagItem {
+  @ApiProperty({ title: '是否显示小圆点', default: false })
+  dot: boolean;
 
-  @ApiProperty({ title: '菜单Icon' })
-  @IsNotEmpty({ message: 'Icon不能为空' })
+  @ApiProperty({ title: '内容' })
+  content: string;
+
+  @ApiProperty({ title: '类型' })
+  type: 'error' | 'primary' | 'warn' | 'success';
+}
+
+// 菜单mete项
+class MetaItem {
+  @ApiProperty({ title: '菜单栏icon' })
   icon: string;
 
-  @ApiProperty({ title: '是否缓存', default: false })
-  keepAlive: boolean;
+  @ApiProperty({ title: 'Tag', type: TagItem })
+  tag?: TagItem;
 
-  @ApiProperty({ title: '上级菜单', default: null })
-  parentId: string | null;
+  @ApiProperty({ title: '是否禁用', default: false })
+  disabled?: boolean;
 
-  @ApiProperty({ title: '排序', example: 0, default: 1 })
-  sort: number;
+  @ApiProperty({ title: '是否隐藏菜单', default: false })
+  hideMenu?: boolean;
 
-  @ApiProperty({ title: '文件路径' })
-  viewUrl: string;
+  @ApiProperty({ title: '菜单名称' })
+  @IsNotEmpty({ message: '菜单名称不能为空' })
+  title: string;
+
+  @ApiProperty({ title: '是否固定标签', default: true })
+  affix?: boolean;
+
+  @ApiProperty({ title: '是否忽略KeepAlive缓存', default: false })
+  ignoreKeepAlive?: boolean;
+
+  @ApiProperty({ title: '隐藏所有子菜单', default: false })
+  hideChildrenInMenu?: boolean;
+
+  @ApiProperty({ title: '当前激活的菜单。用于配置详情页时左侧激活的菜单路径' })
+  currentActiveMenu?: string;
+
+  @ApiProperty({ title: '菜单排序，只对第一级有效', default: 1 })
+  orderNo?: number;
+}
+export class CreateMenuDto {
+  @ApiProperty({ title: '路由名称' })
+  @IsNotEmpty({ message: '路由名称不能为空' })
+  name: string;
 
   @ApiProperty({ title: '路由路径' })
+  @IsNotEmpty({ message: '路由路径不能为空' })
   path: string;
 
-  @ApiProperty({ title: '是否隐藏菜单' })
-  hideMenu: boolean;
+  @ApiProperty({ title: '组件路径' })
+  @IsNotEmpty({ message: '组件路径不能为空' })
+  component: string;
+
+  @ApiProperty({ title: '菜单mate', type: MetaItem })
+  meta: MetaItem;
+
+  @ApiProperty({ title: '上级id', default: null })
+  parentId: string | null;
 }
--- libs/db/src/modules/menu.model.ts
+++ libs/db/src/modules/menu.model.ts
@@ -4,11 +4,68 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-03-25 11:29:25
- * @LastEditTime: 2022-03-25 11:31:25
+ * @LastEditTime: 2022-03-25 18:12:29
  * @Description: Modify here please
  */
 import { ApiProperty } from '@nestjs/swagger';
 import { ModelOptions, prop, Ref } from '@typegoose/typegoose';
+
+class TagItem {
+  @ApiProperty({ title: '是否显示小圆点' })
+  @prop({ type: Boolean, default: false })
+  dot: boolean;
+
+  @ApiProperty({ title: '内容' })
+  @prop()
+  content: string;
+
+  @ApiProperty({ title: '类型' })
+  @prop()
+  type: 'error' | 'primary' | 'warn' | 'success';
+}
+
+// 菜单mete项
+class MetaItem {
+  @ApiProperty({ title: '菜单栏icon' })
+  @prop()
+  icon: string;
+
+  @ApiProperty({ title: '菜单Tag' })
+  @prop({ type: TagItem })
+  tag?: TagItem;
+
+  @ApiProperty({ title: '是否禁用' })
+  @prop({ type: Boolean, default: false })
+  disabled?: boolean;
+
+  @ApiProperty({ title: '是否隐藏菜单' })
+  @prop({ default: false, type: Boolean })
+  hideMenu?: boolean;
+
+  @ApiProperty({ title: '菜单名称' })
+  @prop()
+  title: string;
+
+  @ApiProperty({ title: '是否固定标签' })
+  @prop({ default: true, type: Boolean })
+  affix?: boolean;
+
+  @ApiProperty({ title: '是否忽略KeepAlive缓存' })
+  @prop({ default: false, type: Boolean })
+  ignoreKeepAlive?: boolean;
+
+  @ApiProperty({ title: '隐藏所有子菜单' })
+  @prop({ default: false, type: Boolean })
+  hideChildrenInMenu?: boolean;
+
+  @ApiProperty({ title: '当前激活的菜单。用于配置详情页时左侧激活的菜单路径' })
+  @prop()
+  currentActiveMenu?: string;
+
+  @ApiProperty({ title: '菜单排序，只对第一级有效' })
+  @prop({ type: Number, default: 1 })
+  orderNo?: number;
+}
 
 // 创建时间，更新时间
 @ModelOptions({
@@ -16,36 +73,25 @@ import { ModelOptions, prop, Ref } from '@typegoose/typegoose';
     timestamps: true,
   },
 })
+// 菜单标签设置
 export class Menu {
-  @ApiProperty({ title: '菜单名称' })
+  @ApiProperty({ title: '路由名称' })
   @prop()
   name: string;
-
-  @ApiProperty({ title: '菜单栏icon' })
-  @prop()
-  icon: string;
-
-  @ApiProperty({ title: '是否缓存', example: 0, default: 0 })
-  @prop({ default: 0 })
-  keepAlive: number;
-
-  @ApiProperty({ title: '上级id', default: null })
-  @prop({ ref: () => Menu, default: null })
-  parentId: Ref<Menu> | null;
-
-  @ApiProperty({ title: '排序', example: 0, default: 0 })
-  @prop()
-  sort: number;
-
-  @ApiProperty({ title: '文件路径' })
-  @prop()
-  viewUrl: string;
 
   @ApiProperty({ title: '路由路径' })
   @prop()
   path: string;
 
-  @ApiProperty({ title: '是否隐藏菜单' })
-  @prop({ default: false })
-  hideMenu: boolean;
+  @ApiProperty({ title: '组件路径' })
+  @prop()
+  component: string;
+
+  @ApiProperty({ title: '菜单mate' })
+  @prop({ type: MetaItem })
+  meta: MetaItem;
+
+  @ApiProperty({ title: '上级id', default: null })
+  @prop({ ref: () => Menu, default: null })
+  parentId: Ref<Menu> | null;
 }
