3424b9de1536742eb95327c1683a18474491f4ee
xuanyu <969718197@qq.com>
2022-03-28 16:01:34 +0800

    fix(role): 修复角色管理列表查询接口错误问题

--- apps/admin/src/role/dto/query-role.dto.ts
+++ apps/admin/src/role/dto/query-role.dto.ts
@@ -3,14 +3,14 @@
  * @LastEditors: xuanyu
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
- * @Date: 2022-03-28 14:22:03
- * @LastEditTime: 2022-03-28 15:46:53
+ * @Date: 2022-03-28 15:43:30
+ * @LastEditTime: 2022-03-28 15:46:24
  * @Description: Modify here please
  */
 import { ApiProperty } from '@nestjs/swagger';
 import { PaginationParametersDto } from 'libs/common/PaginationParametersDto';
 
-export class QueryMenuDto extends PaginationParametersDto {
-  @ApiProperty({ title: '菜单名称' })
-  title: string;
+export class QueryRoleDto extends PaginationParametersDto {
+  @ApiProperty({ title: '角色名称' })
+  name: string;
 }
--- apps/admin/src/role/role.service.ts
+++ apps/admin/src/role/role.service.ts
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-03-25 11:46:36
- * @LastEditTime: 2022-03-25 11:54:01
+ * @LastEditTime: 2022-03-28 16:00:28
  * @Description: Modify here please
  */
 import { Injectable } from '@nestjs/common';
@@ -12,6 +12,7 @@ import { ReturnModelType } from '@typegoose/typegoose';
 import { Role } from 'libs/db/modules/role.model';
 import { InjectModel } from 'nestjs-typegoose';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { QueryRoleDto } from './dto/query-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
@@ -26,8 +27,26 @@ export class RoleService {
   }
 
   // 角色列表
-  async findAll() {
-    return await this.roleModel.find();
+  async findAll(parameters: QueryRoleDto) {
+    let total = 0;
+    const result = await this.roleModel
+      .find({
+        $or: [
+          {
+            name: { $regex: new RegExp(parameters.name, 'i') },
+          },
+        ],
+      })
+      .limit(~~parameters.pageSize)
+      .skip(~~((parameters.pageNumber - 1) * parameters.pageSize))
+      .then((doc) => {
+        total = doc.length;
+        return doc;
+      });
+    return {
+      total,
+      items: result,
+    };
   }
 
   // 角色详细
